be04f580c7392e3e16b4c4ae2ab9d844b5646bc8
ssori <115159126+ssori0421@users.noreply.github.com>
2023-08-01 19:57:45 +0900

    feat: 필요한 폴더 및 파일 생성

---
+++ src/app/(order)/order-details/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const OrderDetails = ({ params }) => {
+  const { id } = params;
+
+  return <div>{id}</div>;
+};
+
+export default OrderDetails;
---
+++ src/components/autoSigninCheckbox/AutoSigninCheckbox.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AutoSigninCheckbox = () => {
+  return <div>AutoSigninCheckbox</div>;
+};
+
+export default AutoSigninCheckbox;
