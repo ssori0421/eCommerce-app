ff0e0eb8ee95ff9dee863c28a937582d818a3f68
ssori <115159126+ssori0421@users.noreply.github.com>
2023-08-03 23:52:14 +0900

    feat: useRouter를 사용해서 리다이렉트

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -1,5 +1,6 @@
 'use client';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import LogoPath from '@/assets/colorful.svg';
 
@@ -8,6 +9,14 @@ const LoginClient = () => {
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [isQutoLogin, setIsAutoLogin] = useState(false);
+
+  // router 객체 생성
+  const router = useRouter();
+
+  // push()메서드를 사용해서 원하는 path로 라우팅
+  const redirectUser = () => {
+    router.push('/');
+  };
 
   return (
     <section>
