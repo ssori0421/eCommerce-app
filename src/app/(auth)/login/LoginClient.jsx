5ca0617dbe1df5e0a779271dd10d53a5141f8798
ssori <115159126+ssori0421@users.noreply.github.com>
2023-08-04 10:17:17 +0900

    feat: scss를 사용해서 style 작성

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -1,8 +1,9 @@
 'use client';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import LogoPath from '@/assets/colorful.svg';
+import styles from './Auth.module.scss';
 
 const LoginClient = () => {
   const [email, setEmail] = useState('');
@@ -32,16 +33,26 @@ const LoginClient = () => {
   };
 
   return (
-    <section>
-      <div>
-        <h1>
-          <Image src={LogoPath} alt='logo' />
+    <section className={styles.page}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>
+          <Image src={LogoPath} alt='logo' width={247} />
         </h1>
-        <form onSubmit={loginUser}>
+        <form className={styles.form} onSubmit={loginUser}>
           {/* Input */}
-          <div>{/* 자동 로그인, 비밀번호 수정 */}</div>
-          <div>{/* Button */}</div>
-          <div>{/* Button */}</div>
+          Input
+          <div className={styles.group}>
+            {/* 자동 로그인, 비밀번호 수정 */}
+            자동 로그인, 비밀번호 수정
+          </div>
+          <div className={styles.buttonGroup}>
+            {/* Button */}
+            Button
+          </div>
+          <div>
+            {/* Button */}
+            Button
+          </div>
         </form>
       </div>
     </section>
