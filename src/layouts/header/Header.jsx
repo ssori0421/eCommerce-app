53c8d9add86f7bfc76538f30c6c50033c286a1e1
ssori <115159126+ssori0421@users.noreply.github.com>
2023-09-07 21:38:14 +0900

    feat: 로그인시 유저 데이터를 Redux 스토어에 담기

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -6,14 +6,19 @@ import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from '@/firebase/firebase';
 import { usePathname } from 'next/navigation';
 import InnerHeader from '../innerHeader/InnerHeader';
+import { useDispatch } from 'react-redux';
+import { REMOVE_ACTIVE_USER, SET_ACTIVE_USER } from '@/redux/slice/authSlice';
 
 const Header = () => {
   const pathname = usePathname();
+  // dispatch 함수 가져오기
+  const dispatch = useDispatch();
   const [displayName, setDisplayName] = useState('');
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       console.log('user', user);
       if (user) {
+        // displayName이 없는 경우 email을 이용해서 displayName을 만들어줌
         if (user.displayName === null) {
           const u1 = user.email.substring(0, user.email.indexOf('@'));
           const uName = u1.charAt(0).toUpperCase() + u1.slice(1);
@@ -23,13 +28,25 @@ const Header = () => {
           setDisplayName(user.displayName);
         }
         // 유저 정보를 리덕스 스토어에 저장하기
+        // dispatch 함수를 사용해서 액션 생성자 함수를 reducer에 전달
+        dispatch(
+          SET_ACTIVE_USER({
+            // 아래의 인자들은 action.payload로 들어감
+            email: user.email,
+            userName: user.displayName ? user.displayName : displayName,
+            userID: user.uid,
+          })
+        );
       } else {
         // 로그아웃한 유저
         setDisplayName('');
         // 유저 정보를 리덕스 스토어에서 지우기
+        // dispatch 함수를 사용해서 액션 생성자 함수를 reducer에 전달
+        dispatch(REMOVE_ACTIVE_USER());
       }
     });
-  }, []);
+    // dispatch나 displayName가 바뀌면 useEffect 내부가 다시 호출되도록 함
+  }, [dispatch, displayName]);
 
   // firebase를 사용해서 로그아웃 함수 작성
   const logoutUser = () => {
