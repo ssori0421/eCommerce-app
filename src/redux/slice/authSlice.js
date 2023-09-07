10126678ee2ce63c99f3d72ca31cfff77b0f39c7
ssori <115159126+ssori0421@users.noreply.github.com>
2023-09-07 19:21:03 +0900

    feat: authSlice 작성

---
+++ src/redux/slice/authSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isLoggedIn: false,
+  email: null,
+  userName: null,
+  userID: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  // 액션 생성자 함수
+  reducers: {
+    // 로그인 한 유저
+    SET_ACTIVE_USER: (state, action) => {
+      const { email, userName, userID } = action.payload;
+      state.isLoggedIn = true;
+      state.email = email;
+      state.userName = userName;
+      state.userID = userID;
+    },
+    // 로그아웃 한 유저
+    REMOVE_ACTIVE_USER: (state) => {
+      state.isLoggedIn = false;
+      state.email = null;
+      state.userName = null;
+      state.userID = null;
+    },
+  },
+});
+
+// 구조 분해 할당으로 export
+export const { SET_ACTIVE_USER, REMOVE_ACTIVE_USER } = authSlice.actions;
+
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+export const selectEmail = (state) => state.auth.email;
+export const selectUserName = (state) => state.auth.userName;
+export const selectUserID = (state) => state.auth.userID;
+
+export default authSlice.reducer;
