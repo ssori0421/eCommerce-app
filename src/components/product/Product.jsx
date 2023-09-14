3bd1206269047975bb65cf245224dd24aa467baa
ssori <115159126+ssori0421@users.noreply.github.com>
2023-09-14 16:05:04 +0900

    feat: product 컴포넌트에 useFetchCollection 적용 및 UI 작성

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,7 +1,21 @@
+'use client';
 import React from 'react';
+import useFetchCollection from '@/hooks/useFetchCollection';
+import styles from './Product.module.scss';
 
 const Product = () => {
-  return <div></div>;
+  // useFetchCollection() 커스텀 훅 호출
+  // firebase의 Firestore Database의 'products' 컬렉션을 인자로 전달
+  // 반환값인 data와 isLoading을 받음
+  // const { data, isLoading } = useFetchCollection('products');
+
+  return (
+    <section className={styles.product}>
+      {/* aside 태그를 사용해서 사이드바 UI */}
+      <aside className={styles.filter}></aside>
+      <div className={styles.content}></div>
+    </section>
+  );
 };
 
 export default Product;
