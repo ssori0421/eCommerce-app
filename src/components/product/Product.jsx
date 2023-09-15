c738810b3152c517530cca020dd0c3a9e5d6be7f
ssori <115159126+ssori0421@users.noreply.github.com>
2023-09-15 17:04:27 +0900

    feat: dispatch 함수를 사용해서 상품데이터를 redux store에 저장

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,13 +1,29 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import useFetchCollection from '@/hooks/useFetchCollection';
 import styles from './Product.module.scss';
+import { useDispatch } from 'react-redux';
+import { STORE_PRODUCTS } from '@/redux/slice/productSlice';
 
 const Product = () => {
   // useFetchCollection() 커스텀 훅 호출
   // firebase의 Firestore Database의 'products' 컬렉션을 인자로 전달
   // 반환값인 data와 isLoading을 받음
-  // const { data, isLoading } = useFetchCollection('products');
+  const { data, isLoading } = useFetchCollection('products');
+
+  const dispatch = useDispatch();
+
+  // useEffect 사용
+  // 자동 실행
+  useEffect(() => {
+    // dispatch() 함수
+    // STORE_PRODUCTS 액션 생성자 함수를 인자로 담아서 reducer() 함수에 전달
+    dispatch(
+      STORE_PRODUCTS({
+        products: data,
+      })
+    );
+  }, [data, dispatch]);
 
   return (
     <section className={styles.product}>
