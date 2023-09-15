0310e863f4573b3ff29c81aeab70665982e4ed25
ssori <115159126+ssori0421@users.noreply.github.com>
2023-09-15 17:03:26 +0900

    feat: redux store에 productReducer 추가

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,16 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from './slice/authSlice';
+import productReducer from './slice/productSlice';
 
 // rootReducer 선언
 // combineReducers 함수 사용
 // 다양한 reducer들을 합쳐서 하나의 객체로 반환
 const rootReducer = combineReducers({
   auth: authReducer,
+  product: productReducer,
 });
 
+// Redux Store 정의
 // configureStore 함수 사용
 // store생성 및 다양한 설정
 const store = configureStore({
