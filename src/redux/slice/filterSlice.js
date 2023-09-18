0256da81690092c3e374a9fecf1641c47580526d
ssori <115159126+ssori0421@users.noreply.github.com>
2023-09-18 15:43:21 +0900

    docs: 상품 나열 필터 기능 관련 filterSlice 파일 작성

---
+++ src/redux/slice/filterSlice.js
@@ -0,0 +1,77 @@
+import { createAction, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  filteredProducts: [],
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    FILTER_BY_CATEGORY: (state, action) => {
+      const { products, category } = action.payload;
+      let tempProducts = [];
+      if (category === 'All') {
+        tempProducts = products;
+      } else {
+        tempProducts = products.filter(
+          (product) => product.category === category
+        );
+      }
+      state.filteredProducts = tempProducts;
+    },
+    FILTER_BY_BRAND: (state, action) => {
+      const { products, price } = action.payload;
+      let tempProducts = [];
+      if (brand === 'All') {
+        tempProducts = [products];
+      } else {
+        tempProducts = products.filter((product) => product.brand === brand);
+      }
+      state.filteredProducts = tempProducts;
+    },
+    FILTER_BY_PRICE: (state, action) => {
+      const { products, price } = action.payload;
+      let tempProducts = [];
+      // filter() 메서드
+      // 현재 설정한 가격보다 작거나 같은 것들만 담아서 새로운 배열로 반환해서 tempProducts에 할당
+      tempProducts = products.filter((prodict) => prodict.price <= price);
+      // tempProducts를 initialState의 filteredProducts에 할당
+      state.filteredProducts = tempProducts;
+    },
+    FILTER_BY: (state, action) => {
+      const { products, price, brand, category } = action.payload;
+      let tempProducts = [];
+
+      if (category === 'All') {
+        tempProducts = products;
+      } else {
+        tempProducts = products.filter(
+          (product) => product.category === category
+        );
+      }
+      if (brand === 'All') {
+        tempProducts = tempProducts;
+      } else {
+        tempProducts = tempProducts.filter(
+          (product) => product.brand === brand
+        );
+      }
+
+      tempProducts = tempProducts.filter((product) => product.price <= price);
+
+      state.filteredProducts = tempProducts;
+    },
+  },
+});
+
+export const {
+  FILTER_BY_BRAND,
+  FILTER_BY_CATEGORY,
+  FILTER_BY_PRICE,
+  FILTER_BY,
+} = filterSlice.actions;
+
+export const selectFilteredProducts = (state) => state.filter.filteredProducts;
+
+export default filterSlice.reducer;
