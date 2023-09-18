6b4c37cd1b422dc3fde2c9f2c7aaa1168876d348
ssori <115159126+ssori0421@users.noreply.github.com>
2023-09-18 17:42:08 +0900

    chore: 주석 추가

--- src/redux/slice/filterSlice.js
+++ src/redux/slice/filterSlice.js
@@ -1,13 +1,23 @@
-import { createAction, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
+// initialState 정의
 const initialState = {
   filteredProducts: [],
 };
 
+// filterSlice 정의 (createSlice ()메서드 사용해 slice 생성)
 const filterSlice = createSlice({
+  // ⭐️ 첫 번째 인자: name => Redux Store의 상태 객체에 접근할 수 있는 키(key)로 사용
   name: 'filter',
+  // ⭐️ 두 번째 인자: state => initialState를 사용함
   initialState,
+  // ⭐️ 세 번째 인자: reducer 함수
+  // 액션 생성자 함수가 반환한 값으로 Redux Store의 state 상태 객체의 값을 변경
   reducers: {
+    // ⚠️ 액션 생성자 함수
+    // 파라미터로 state(initialState)와 action을 받음
+    // 액션 생성자 함수에 전달한 값은 action.payload에 담김 - 액션 객체(key:value)
+    // 새로운 state 상태 객체를 반환
     FILTER_BY_CATEGORY: (state, action) => {
       const { products, category } = action.payload;
       let tempProducts = [];
@@ -65,6 +75,7 @@ const filterSlice = createSlice({
   },
 });
 
+// 구조 분해 할당으로 export
 export const {
   FILTER_BY_BRAND,
   FILTER_BY_CATEGORY,
@@ -72,6 +83,11 @@ export const {
   FILTER_BY,
 } = filterSlice.actions;
 
+// 함수 작성
+// 💡 Redux Store에 왜 값을 저장했지? => 필요시에 useSelector() 메서드를 사용해서 Redux Store에 저장된 state를 가져다가 쓰기 위해!!
+// state는 Redux Store의 상태 객체
+// name(key)를 사용해서 상태 객체 state에서 filterSlice애 점근 >
+// filterSlice의 여러 필드에 쉽게 접근 가능
 export const selectFilteredProducts = (state) => state.filter.filteredProducts;
 
 export default filterSlice.reducer;
