834e37a5ef4cf04d7c72d63cda06d73d5f44c6f1
ssori <115159126+ssori0421@users.noreply.github.com>
2023-09-25 19:56:06 +0900

    feat: ProductItem 컴포넌트 UI 작업중

--- src/components/product/productItem/ProductItem.jsx
+++ src/components/product/productItem/ProductItem.jsx
@@ -1,7 +1,35 @@
+'use client';
 import React from 'react';
+import styles from './ProductItem.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
 
-const ProductItem = () => {
-  return <div>ProductItem</div>;
+const ProductItem = ({ id, name, price, imageURL }) => {
+  const shortenText = (text, n) => {
+    // name의 길이가 n 이상인 경우 n개까지 자르고 말줄임표
+    if (text.length > n) {
+      const shortenedText = text.substring(0, n).concat('...');
+      return shortenedText;
+    }
+    // 그렇지 않은 경우 그대로 사용
+    return text;
+  };
+
+  return (
+    <div className={styles.grid}>
+      <Link href={`/product-details/${id}`}>
+        <div className={styles.img}>
+          <Image src={imageURL} alt={name} width={265} height={265} />
+        </div>
+      </Link>
+      <div className={styles.content}>
+        <div className={styles.details}>
+          {/* shortenText 함수를 사용해서 name을 최대 10글자로 줄임 */}
+          <p>{shortenText(name, 10)}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductItem;
