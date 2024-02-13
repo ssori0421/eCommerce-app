f02f4238aae56866858231e87da55d2e3b8e7213
ssori <115159126+ssori0421@users.noreply.github.com>
2024-02-13 21:16:30 +0900

    feat: orderSlice reducer 추가

--- src/redux/slice/orderSlice.ts
+++ src/redux/slice/orderSlice.ts
@@ -14,19 +14,31 @@ const initialState: IOrderState = {
 
 const orderSlice = createSlice({
   name: 'orders',
-
   initialState,
-
   reducers: {
     STORE_ORDERS(state, action) {
       state.orderHistory = action.payload;
     },
+    CALCULATE_TOTAL_ORDER_AMOUNT(state) {
+      const array: number[] = [];
+      state.orderHistory.map((item) => {
+        const { orderAmount } = item;
+        return array.push(orderAmount);
+      });
+      const totalAmount = array.reduce((a, b) => {
+        return a + b;
+      }, 0);
+      state.totalOrderAmount = totalAmount;
+    },
   },
 });
 
-export const { STORE_ORDERS } = orderSlice.actions;
+export const { STORE_ORDERS, CALCULATE_TOTAL_ORDER_AMOUNT } =
+  orderSlice.actions;
 
 export const selectOrderHistory = (state: RootState) =>
   state.orders.orderHistory;
+export const selectTotalOrderAmount = (state: RootState) =>
+  state.orders.totalOrderAmount;
 
 export default orderSlice.reducer;
